ac46363b7e8d5e8a8421e7c3280d374e073435ef
Robin Punnoose <robin.punnoose@protonmail.com>
2023-01-06 23:30:18 -0600

    added links for component bar, added react-icons library

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,11 +1,46 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Navbar() {
   return (
     <div className='fixed w-full h-20 shadow-xl z-[100]'>
         <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
-            <Image width={100} height={100} src="/../public/assets/logo.png" alt="logo"/>
+            <Image
+              width={125}
+              height={125}
+              src="/../public/assets/logo.png"
+              alt="logo"
+              />
+              <div>
+                <ul className='hidden md:flex'>
+                    <li className='ml-10 font-extrabold text-sm uppercase hover:border-b'>
+                      <Link href='/'>
+                        Home
+                      </Link>
+                    </li>
+                    <li className='ml-10 font-extrabold text-sm uppercase hover:border-b'>
+                      <Link href='/'>
+                        About
+                      </Link>
+                    </li>
+                    <li className='ml-10 font-extrabold text-sm uppercase hover:border-b'>
+                      <Link href='/'>
+                        Skills
+                      </Link>
+                      </li>
+                    <li className='ml-10 font-extrabold text-sm uppercase hover:border-b'>
+                      <Link href='/'>
+                        Projects
+                      </Link>
+                    </li>
+                    <li className='ml-10 font-extrabold text-sm uppercase hover:border-b'>
+                      <Link href='/'>
+                        Contact
+                      </Link>
+                    </li>
+                </ul>
+              </div>
         </div>
     </div>
   )
