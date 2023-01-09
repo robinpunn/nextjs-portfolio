2f63fb7c10edad19206855d010f81e880105a9a9
Robin Punnoose <robin.punnoose@protonmail.com>
2023-01-09 12:36:05 -0600

    added project images, completed project section

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -2,15 +2,20 @@ import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 
-const ProjectCard = ({title,tech,description,img,link}) => {
+const ProjectCard = ({title,tech,img,link}) => {
   return (
-    <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-600 rounded-xl p-4 group hover:bg-gradient-to-r from-violet-500 to-fuchsia-500'>
-        <Image className="rounded-xl group-hover:opacity-10" src={img} alt='/'/>
+    <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-600 rounded-xl group hover:bg-gradient-to-r from-violet-500 to-fuchsia-500'>
+        <Image
+            className="rounded-xl group-hover:opacity-10"
+            src={img}
+            width={1200}
+            height={1000}
+            alt={title}
+        />
         <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
             <h3 className='text-2xl text-white tracking-wider text-center'>{title}</h3>
             <p className='pb-4 pt-2 text-white text-center'>{tech}</p>
-            <p className='pb-4 pt-2 text-white text-center'>{description}</p>
-            <p className='text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer'>
+            <p className='w-32 h-8 mx-auto text-center rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer'>
                 <Link href={link}>More Info</Link>
             </p>
         </div>
--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,24 +1,52 @@
 import React from 'react'
 import ProjectCard from './ProjectCard'
 
+
 const Projects = () => {
     {/*Add Projects Here*/}
     const projects = [
         {
             title: 'TradeBot',
             tech: 'HTML/CSS',
-            description: 'This is a landing page that used HTML and CSS with minimal JavaScript. It incorporates CSS FLexbox and Grid along with transitions and animations. ',
-            img: '/../'
+            img: '/../public/assets/projects/trade.png',
+            link: '/'
         },
-
+        {
+            title: 'NextJs Blog',
+            tech: 'NextJs/WordPress/GraphQl/Tailwind',
+            img: '/../public/assets/projects/blog.png',
+            link: '/'
+        },
+        {
+            title: 'ZTM Portfolio',
+            tech: 'JavaScript/Node/React/Bootstrap/SQL',
+            img: '/../public/assets/projects/ztm.png',
+            link: '/'
+        },
+        {
+            title: 'freeCodeCamp Portfolio',
+            tech: 'HTML/CSS',
+            img: '/../public/assets/projects/free.png',
+            link: '/'
+        },
+        {
+            title: 'Email Developer Portfolio',
+            tech: 'HTML Email Frameworks',
+            img: '/../public/assets/projects/email.png',
+            link: '/'
+        },
     ]
+
   return (
     <div className='w-full'>
         <div className='max-w-[1240px] mx-auto px-2 py-16'>
             <p className='uppercase text-xl tracking-widest'>Projects</p>
             <h2 className='py-4'>What I've Built</h2>
             <div className='grid md:grid-cols-2 gap-8'>
-             <ProjectCard />
+              {/*projects placement, cycle through array, pass to ProjectCard component*/}
+              {projects.map(project => (
+                <ProjectCard title={project.title} tech={project.tech} img={project.img} link={project.link} />
+            ))}
             </div>
         </div>
     </div>
